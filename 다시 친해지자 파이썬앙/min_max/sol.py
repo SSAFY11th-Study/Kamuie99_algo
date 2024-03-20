5ffaf0f5af16106c9584084596ea79daa5e7fed1
Kamuie99 <dakgoo02@naver.com>
2024-03-20 17:17:33 +0900

    0320

--- 다시 친해지자 파이썬앙/min_max/sol.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.stdin = open('input.txt')
-
-T = int(input())
-for test_case in range(1, T+1):
-  N = int(input())
-  field = list(map(int, input().split()))
-  print(f'#{test_case} {max(field)-min(field)}')
